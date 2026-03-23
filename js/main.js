2dc1bf8163b31b72158ceface10100bb9844e0ae
Mohandahss S <mohandhass123s@gmail.com>
2026-03-24 00:14:23 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ const LETTER_CONFIG = {
         targetIndex: 6,  // 7th target in targets.mind (assuming you compiled A-J together)
         models: {
             'gift': { file: 'gift.glb', scale: '4 4 4', label: 'Gift', default: true },
-            'goat': { file: 'goat.glb',  position: '-10 0 0', scale: '0.5 0.5 0.5', label: 'Goat' },
+            'goat': { file: 'goat.glb',  position: '-50 0 0', scale: '0.5 0.5 0.5', label: 'Goat' },
             'grape': { file: 'grape.glb', scale: '0.1 0.1 0.1', label: 'Grape' }
         }
     }
